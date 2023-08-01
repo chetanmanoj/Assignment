eb5347dda45e93e5e7508cb31f1774f111785d7a
chetanmanoj <chetanmanoj2001@gmail.com>
2023-08-01 22:21:18 +0530

    Built header

---
+++ public/assets/index.js
@@ -0,0 +1,8 @@
+import lines from './lines.svg'
+import dollar from './cashapp/logo.svg'
+import eye from './eye.svg'
+
+export{
+    dollar,
+    eye,
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,34 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 
-import lines from '../assets/lines.svg'
+import {dollar, eye} from '../../public/assets'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen items-center justify-center object-contain ${inter.className}`}
+      className={`flex flex-row min-h-screen bg-main-bg justify-center ${inter.className}`}
     >
-     <Image src={lines} alt="React" className='' />
+
+      {/* HEADER */}
+    <div className='flex flex-row h-[5.5rem] items-center justify-between px-[3.75rem] w-full'>
+      <Image src={dollar} alt="No moni" className='flex items-start'/>
+      <div className='flex justify-around gap-[1.25rem] font-og font-extrabold text-xs tracking-[.03125rem]'>
+        <span>SIGN IN</span>
+        <span>LEGAL</span>
+        <span>LICENSES</span>
+        <span>SECURITY</span>
+        <span>CAREERS</span>
+        <span>PRESS</span>
+        <span>SUPPORT</span>
+        <span>STATUS</span>
+        <span>CODEBLOG</span>
+      </div>
+      <Image src={eye} alt="No moni"/>
+    </div>
+
+    
     </main>
   )
 }
